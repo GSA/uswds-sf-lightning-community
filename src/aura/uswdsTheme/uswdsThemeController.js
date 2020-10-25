2eaa4b78b161a44a003d06d5618367c993139c47
Mark Vogelgesang <mark.vogelgesang@gsa.gov>
2020-10-25 15:24:00 -0400

    Issue 28 (#32)
    * #28 created space for shopping cart component that collapses when not in use. Updated styling to support grid layout and to collapse section to the right when login is not displayed

--- src/aura/uswdsTheme/uswdsThemeController.js
+++ src/aura/uswdsTheme/uswdsThemeController.js
@@ -1,13 +1,19 @@
 ({
   doInit: function (cmp, event, helper) {
-    var toggle = cmp.get("v.footerEmailSignUpToggle");
-    if (toggle == "On") {
+    var footerEmailSignUpToggle = cmp.get("v.footerEmailSignUpToggle");
+    if (footerEmailSignUpToggle == "On") {
       var footerSecondary1 = cmp.find("footer_secondary_1");
       var footerSecondary2 = cmp.find("footer_secondary_2");
       $A.util.removeClass(footerSecondary1, "mobile-lg:grid-col-6");
       $A.util.removeClass(footerSecondary2, "mobile-lg:grid-col-6");
       $A.util.addClass(footerSecondary1, "mobile-lg:grid-col-4");
       $A.util.addClass(footerSecondary2, "mobile-lg:grid-col-4");
+    }
+
+    var loginToggle = cmp.get("v.displayLogin");
+    if (loginToggle == "No") {
+      var shoppingCart = cmp.find("shoppingCart");
+      $A.util.addClass(shoppingCart, "grid-offset-10");
     }
   }
 });
