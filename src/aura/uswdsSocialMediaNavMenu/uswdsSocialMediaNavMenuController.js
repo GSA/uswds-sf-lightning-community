de2dee436054a2e5796ce50edc0b4f62a0e884f0
rnangunoori-r7 <60349509+rnangunoori-r7@users.noreply.github.com>
2021-03-19 15:19:33 -0400

    "#50 Fix Social Media links on the footer"

--- src/aura/uswdsSocialMediaNavMenu/uswdsSocialMediaNavMenuController.js
+++ src/aura/uswdsSocialMediaNavMenu/uswdsSocialMediaNavMenuController.js
@@ -1,7 +1,7 @@
 ({
   doInit: function (cmp, event, helper) {},
   onClick: function (component, event, helper) {
-    var id = event.target.dataset.menuItemId;
+    var id = event.currentTarget.dataset.rowIndex;
     if (id) {
       component.getSuper().navigate(id);
     }
