164fcfd6e02c18ae9ef4a373dc9bb7151cc8acd8
Mark Vogelgesang <mark.vogelgesang@gsa.gov>
2021-09-09 21:25:43 -0400

    #83 created uswds Card component along with example wrapper component to show utilization and navigation

---
+++ example/aura/exampleCardWrapper/exampleCardWrapperController.js
@@ -0,0 +1,5 @@
+({
+  doInit: function (component, event, helper) {
+    component.set("v.cardData", helper.generateData());
+  }
+});
---
+++ example/aura/exampleCardWrapper/exampleCardWrapperHelper.js
@@ -0,0 +1,127 @@
+({
+  generateData: function () {
+    return [
+      {
+        cardType: "Card",
+        cardsPerRow: "3",
+        header: "Card",
+        body:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        buttonLinkUrl: "/contactsupport",
+        buttonLinkText: "Contact Support",
+        mediaUrl: $A.get("$ContentAsset.ex_uswdsCard"),
+        mediaAltText: "A placeholder image"
+      },
+      {
+        cardType: "Card with Media",
+        cardsPerRow: "3",
+        header: "Card with Media",
+        body:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ",
+        buttonLinkUrl: "/contactsupport",
+        buttonLinkText: "Contact Suppoert",
+        mediaUrl: $A.get("$ContentAsset.ex_uswdsCard"),
+        mediaAltText: "A placeholder image"
+      },
+      {
+        cardType: "Media and Header First",
+        cardsPerRow: "3",
+        header: "Media and Header First",
+        body:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        buttonLinkUrl: "",
+        buttonLinkText: "Contact Support",
+        mediaUrl: $A.get("$ContentAsset.ex_uswdsCard"),
+        mediaAltText: "A placeholder image"
+      },
+      {
+        cardType: "Inset Media",
+        cardsPerRow: "2",
+        header: "Inset Media",
+        body:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        buttonLinkUrl: "/topiccatalog",
+        buttonLinkText: "Topic Catalog",
+        mediaUrl: $A.get("$ContentAsset.ex_uswdsCard"),
+        mediaAltText: "A placeholder image"
+      },
+      {
+        cardType: "Exdent Media",
+        cardsPerRow: "2",
+        header: "Exdent Media",
+        body:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        buttonLinkUrl: "/topiccatalog",
+        buttonLinkText: "Topic Catalog",
+        mediaUrl: $A.get("$ContentAsset.ex_uswdsCard"),
+        mediaAltText: "A placeholder image"
+      },
+      {
+        cardType: "Card with Media",
+        cardsPerRow: "4",
+        header: "Card with Media",
+        body:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ",
+        buttonLinkUrl: "/files",
+        buttonLinkText: "Link to Files",
+        mediaUrl: $A.get("$ContentAsset.ex_uswdsCard"),
+        mediaAltText: "A placeholder image"
+      },
+      {
+        cardType: "Card with Media",
+        cardsPerRow: "4",
+        header: "Card with Media",
+        body:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ",
+        buttonLinkUrl: "/files",
+        buttonLinkText: "Link to Files",
+        mediaUrl: $A.get("$ContentAsset.ex_uswdsCard"),
+        mediaAltText: "A placeholder image"
+      },
+      {
+        cardType: "Card with Media",
+        cardsPerRow: "4",
+        header: "Card with Media",
+        body:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ",
+        buttonLinkUrl: "/files",
+        buttonLinkText: "Link to Files",
+        mediaUrl: $A.get("$ContentAsset.ex_uswdsCard"),
+        mediaAltText: "A placeholder image"
+      },
+      {
+        cardType: "Card with Media",
+        cardsPerRow: "4",
+        header: "Card with Media",
+        body:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ",
+        buttonLinkUrl: "/files",
+        buttonLinkText: "Link to Files",
+        mediaUrl: $A.get("$ContentAsset.ex_uswdsCard"),
+        mediaAltText: "A placeholder image"
+      },
+      {
+        cardType: "Flag Left",
+        cardsPerRow: "2",
+        header: "Flag Left",
+        body:
+          "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. ",
+        buttonLinkUrl: "/login",
+        buttonLinkText: "Login",
+        mediaUrl: $A.get("$ContentAsset.ex_uswdsCard"),
+        mediaAltText: "A placeholder image"
+      },
+      {
+        cardType: "Flag Right",
+        cardsPerRow: "2",
+        header: "Flag Right",
+        body:
+          "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        buttonLinkUrl: "/login",
+        buttonLinkText: "Login",
+        mediaUrl: $A.get("$ContentAsset.ex_uswdsCard"),
+        mediaAltText: "A placeholder image"
+      }
+    ];
+  }
+});
