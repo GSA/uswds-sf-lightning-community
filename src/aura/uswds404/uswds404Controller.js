9f4b081162d09023da5f5e220a8ecb8017184dbc
Mark Vogelgesang <mark.vogelgesang@gsa.gov>
2021-12-10 14:18:25 -0500

    #109 added onclick behavior for homepage button, uses urlPattern attribute

--- src/aura/uswds404/uswds404Controller.js
+++ src/aura/uswds404/uswds404Controller.js
@@ -4,6 +4,14 @@
     var content = helper.getContent(language);
     cmp.set("v.pageContent", content);
   },
+  gotoHome: function (cmp, event, helper) {
+    var urlEvent = $A.get("e.force:navigateToURL");
+    var homeUrl = cmp.get("v.urlPattern");
+    urlEvent.setParams({
+      url: homeUrl
+    });
+    urlEvent.fire();
+  },
   gotoForm: function (cmp, event, helper) {
     var urlEvent = $A.get("e.force:navigateToURL");
     var formUrl = cmp.get("v.formUrl");
