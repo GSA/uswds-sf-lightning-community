bfd08879cc67daa125342258fa2756a32f0b94d0
Mark Vogelgesang <mark.vogelgesang@gsa.gov>
2022-08-16 13:57:35 -0400

    #139 code deployed to scratch org.

---
+++ src/aura/uswdsBreadcrumb/uswdsBreadcrumbController.js
@@ -0,0 +1,12 @@
+({
+  doInit: function (component, event, helper) {
+    const wrapClass = component.get("v.wrap") ? "usa-breadcrumb--wrap" : "";
+    component.set("v.wrapClass", wrapClass);
+  },
+  onClick: function (component, event, helper) {
+    var id = event.target.dataset.menuItemId;
+    if (id) {
+      component.getSuper().navigate(id);
+    }
+  }
+});
