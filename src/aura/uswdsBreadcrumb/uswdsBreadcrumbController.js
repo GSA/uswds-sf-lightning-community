377d68e088e483ea175da9e9fbaec8bddf9a4667
Mark Vogelgesang <mark.vogelgesang@gsa.gov>
2022-08-24 13:32:36 -0400

    #139 removed events and child components as they were not necessary

--- src/aura/uswdsBreadcrumb/uswdsBreadcrumbController.js
+++ src/aura/uswdsBreadcrumb/uswdsBreadcrumbController.js
@@ -4,17 +4,12 @@
       component.get("v.wrap") == "Yes" ? "usa-breadcrumb--wrap" : "";
     component.set("v.wrapClass", wrapClass);
   },
-  handleChildComponentInitEvent: function (component, event, helper) {
-    var valueFromChild = event.getParam("label");
-    const breadcrumbList = component.get("v.breadcrumbList");
-    if (breadcrumbList == "") {
-      component.set("v.breadcrumbList", valueFromChild);
-    } else {
-      component.set("v.breadcrumbList", breadcrumbList + "," + valueFromChild);
+  onClick: function (component, event, helper) {
+    console.log("I was clicked!");
+    var id = event.target.dataset.menuItemId;
+    console.log("id", id);
+    if (id) {
+      component.getSuper().navigate(id);
     }
-  },
-  handleChildComponentClickEvent: function (component, event, helper) {
-    var valueFromChild = event.getParam("menuId");
-    component.getSuper().navigate(valueFromChild);
   }
 });
