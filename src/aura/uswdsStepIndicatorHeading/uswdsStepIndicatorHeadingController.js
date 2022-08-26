55e8e192170a8b7773cb12e120f72dc6fd7333e8
Mark Vogelgesang <mark.vogelgesang@gsa.gov>
2022-08-25 23:26:38 -0400

    #142 created initial version of stepIndicatorHeading and the bones of an example component.

--- src/aura/uswdsStepIndicatorHeading/uswdsStepIndicatorHeadingController.js
+++ src/aura/uswdsStepIndicatorHeading/uswdsStepIndicatorHeadingController.js
@@ -1,5 +1,9 @@
 ({
-    myAction : function(component, event, helper) {
+  doInit: function (component, event, helper) {
+    const lang = component.get("v.language");
+    const languageContent = helper.getLanguageContent(component, lang);
+    component.set("v.languageContent", languageContent);
 
-    }
-})
+    helper.validateCounters(component);
+  }
+});
