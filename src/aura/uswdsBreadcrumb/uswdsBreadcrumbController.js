613a7b9ebb5c4f1408f1c8a47c3218c06c657fca
Mark Vogelgesang <mark.vogelgesang@gsa.gov>
2022-08-28 14:08:22 -0400

    Removed/ updated errant console.log statements

--- src/aura/uswdsBreadcrumb/uswdsBreadcrumbController.js
+++ src/aura/uswdsBreadcrumb/uswdsBreadcrumbController.js
@@ -5,9 +5,7 @@
     component.set("v.wrapClass", wrapClass);
   },
   onClick: function (component, event, helper) {
-    console.log("I was clicked!");
     var id = event.target.dataset.menuItemId;
-    console.log("id", id);
     if (id) {
       component.getSuper().navigate(id);
     }
