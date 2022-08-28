a3d57772e03c8b53bed9003eb9b9413f70c8a74a
Mark Vogelgesang <mark.vogelgesang@gsa.gov>
2022-08-28 14:38:45 -0400

    #142 final validation for step indicator segment, PR ready

--- src/aura/uswdsStepIndicatorSegment/uswdsStepIndicatorSegmentController.js
+++ src/aura/uswdsStepIndicatorSegment/uswdsStepIndicatorSegmentController.js
@@ -1,22 +1,6 @@
 ({
   doInit: function (component, event, helper) {
-    const segment = component.get("v.stepIndicatorSegment");
-    var status = "";
-    if (segment.status.toLowerCase() === "current") {
-      component.set(
-        "v.currentStatusClass",
-        "usa-step-indicator__segment--current"
-      );
-    } else if (segment.status.toLowerCase() === "completed") {
-      status = "Completed";
-      component.set(
-        "v.currentStatusClass",
-        "usa-step-indicator__segment--complete"
-      );
-    } else {
-      status = "Not Completed";
-    }
-
-    component.set("v.status", status);
+    helper.updateStatusClass(component);
+    helper.validateLabelHasValue(component);
   }
 });
