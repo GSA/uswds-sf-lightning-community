0ea2e0d4cee9d6594e8ae77ee548dfdea4e4e6b0
Mark Vogelgesang <mark.vogelgesang@gsa.gov>
2022-09-14 11:57:45 -0400

    #154 migrated cardList to builderNotification and created tests.

--- src/aura/uswdsCardList/uswdsCardListHelper.js
+++ src/aura/uswdsCardList/uswdsCardListHelper.js
@@ -17,15 +17,17 @@
         var cardToString = Object.keys(cardData)
           .map((card) => "'" + card + "' : '" + cardData[card] + "'")
           .join("\n");
-        console.warn(
-          `Error - USWDS Card List. \n Error on card no ${index}. Expecting key '${cardFields[i]}' but it was not found. Instead, the following was presented: \n\n ${cardToString}`
+
+        this.createNotification(
+          component,
+          {
+            title: "Error - USWDS Card List",
+            message: `Validation error, cardData did not provide all expected object keys. Specifics have been logged to the developer console for  review.`
+          },
+          [
+            `Error - USWDS Card List. \n Error on card no ${index}. Expecting key '${cardFields[i]}' but it was not found. Instead, the following was presented: \n\n ${cardToString}`
+          ]
         );
-        component.find("notifLib").showToast({
-          title: "Error - USWDS Card List",
-          message: `Validation error, cardData did not provide all expected object keys. Specifics have been logged to the developer console for  review.`,
-          variant: "warning",
-          mode: "sticky"
-        });
       }
     }
     // Check to see that the cardType value is valid
@@ -39,17 +41,28 @@
       "Flag Left"
     ];
     if (!validCardTypes.includes(cardData.cardType)) {
-      console.warn(
-        `Error - USWDS Card List. \n Error on card no ${index}. An invalid cardType value was provided. Value found: ${
-          cardData.cardType
-        }. Valid values: ${validCardTypes.toString()}.`
+      this.createNotification(
+        component,
+        {
+          title: "Error - USWDS Card List",
+          message: `Validation error, invalid cardType value provided. Specific details are logged to the developer console for your review.`
+        },
+        [
+          `Error - USWDS Card List. \n Error on card no ${index}. An invalid cardType value was provided. Value found: ${
+            cardData.cardType
+          }. Valid values: ${validCardTypes.toString()}.`
+        ]
       );
-      component.find("notifLib").showToast({
-        title: "Error - USWDS Card List",
-        message: `Validation error, invalid cardData value provided. Specific details are logged to the developer console for your review.`,
-        variant: "warning",
-        mode: "sticky"
-      });
     }
+  },
+  createNotification: function (
+    component,
+    builderNotificationContent,
+    consoleWarningArray
+  ) {
+    component
+      .find("builderNotification")
+      .addNotification(builderNotificationContent);
+    console.warn(...consoleWarningArray);
   }
 });
