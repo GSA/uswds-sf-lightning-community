036bd0924f760cd79377d9cce321c51342988ca8
Mark Vogelgesang <mark.vogelgesang@gsa.gov>
2023-02-10 11:07:19 -0500

    #181 working prototype

--- src/aura/uswdsHero/uswdsHeroController.js
+++ src/aura/uswdsHero/uswdsHeroController.js
@@ -1,5 +1,8 @@
 ({
-    myAction : function(component, event, helper) {
-
-    }
-})
+  doInit: function (component, event, helper) {
+    var heroImgName = component.get("v.heroImgName");
+    // dynamically fetch contentAsset using name input into heroImg attribute
+    var heroImg = $A.getReference("$ContentAsset." + heroImgName);
+    component.set("v.heroReference", heroImg);
+  }
+});
